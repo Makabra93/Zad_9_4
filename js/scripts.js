30c14440fa079e2384306cfc8ecf7df7adca86c1
Mateusz Lawczys <ma6tek@gmail.com>
2017-12-07 15:52:09 +0100

    Change js content

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 // scripts.js
 
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-var dinosaur = 'Triceratops';
-var upperDinosaur = dinosaur.toUpperCase();
-var textFixed = text.replace('Velociraptor', upperDinosaur);
+function drawTree(treeHeight) {
+    for (var i = 0 ; i < treeHeight ; i++) {
+        var star = '';
+        for (var j = 0 ; j <= i ; j++) {
+            star += '*';
+        }
+    console.log(star);
+    }
+}
 
-
-console.log(textFixed.substr(0, textFixed.length/2));
-
+drawTree(5);
